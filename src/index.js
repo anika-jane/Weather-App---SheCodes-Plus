9800eabaaf89ae13a83969bf0546a0175c752b35
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-02-16 13:30:24 -0500

    I created the repository and then added the files to it.

---
+++ src/index.js
@@ -0,0 +1,77 @@
+let weather = {
+    paris: {
+      temp: 19.7,
+      humidity: 80
+    },
+    tokyo: {
+      temp: 17.3,
+      humidity: 50
+    },
+    lisbon: {
+      temp: 30.2,
+      humidity: 20
+    },
+    "san francisco": {
+      temp: 20.9,
+      humidity: 100
+    },
+    oslo: {
+      temp: -5,
+      humidity: 20
+    }
+  };
+
+
+ 
+  let city = prompt("Enter a city?");
+city = city.toLowerCase();
+if (weather[city] !== undefined) {
+  let temperature = weather[city].temp;
+  let humidity = weather[city].humidity;
+  let celsiusTemperature = Math.round(temperature);
+  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+
+  alert(
+    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+  );
+} else {
+  alert(
+    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+  );
+}
+
+
+
+// date and time //
+
+
+
+let currently = document.querySelector("#currently");
+let currentTime = new Date();
+let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let dayIndex = currentTime.getDay();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+currently.innerHTML = `${days[dayIndex]}, ${hours}:${minutes}`;
+
+
+// 3 //
+
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = cityInput.value;
+}
+
+let searchForm = document.querySelector("#search-form");
+
+searchForm.addEventListener("submit", search);

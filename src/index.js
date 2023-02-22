6ba6d12a6ceec2bb83a8eea53fd670e2429487d8
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-02-22 10:29:20 -0500

    trying to figure this out why do i need to keep going to this???????????

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
-let weather = {
-    paris: {
-      temp: 19.7,
-      humidity: 80
-    },
-    tokyo: {
-      temp: 17.3,
-      humidity: 50
-    },
-    lisbon: {
-      temp: 30.2,
-      humidity: 20
-    },
-    "san francisco": {
-      temp: 20.9,
-      humidity: 100
-    },
-    oslo: {
-      temp: -5,
-      humidity: 20
-    }
-  };
+// let weather = {
+//     paris: {
+//       temp: 19.7,
+//       humidity: 80
+//     },
+//     tokyo: {
+//       temp: 17.3,
+//       humidity: 50
+//     },
+//     lisbon: {
+//       temp: 30.2,
+//       humidity: 20
+//     },
+//     "san francisco": {
+//       temp: 20.9,
+//       humidity: 100
+//     },
+//     oslo: {
+//       temp: -5,
+//       humidity: 20
+//     }
+//   };
 
 
  
-  let city = prompt("Enter a city?");
-city = city.toLowerCase();
-if (weather[city] !== undefined) {
-  let temperature = weather[city].temp;
-  let humidity = weather[city].humidity;
-  let celsiusTemperature = Math.round(temperature);
-  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+//   let city = prompt("Enter a city?");
+// city = city.toLowerCase();
+// if (weather[city] !== undefined) {
+//   let temperature = weather[city].temp;
+//   let humidity = weather[city].humidity;
+//   let celsiusTemperature = Math.round(temperature);
+//   let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
 
-  alert(
-    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
-  );
-} else {
-  alert(
-    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-  );
-}
+//   alert(
+//     `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+//   );
+// } else {
+//   alert(
+//     `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+//   );
+// }
 
 
 
@@ -67,18 +67,12 @@ currently.innerHTML = `${days[dayIndex]}, ${hours}:${minutes}`;
 
 function search(event) {
   event.preventDefault();
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let city = "paris";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let cityInput = document.querySelector("#city-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = cityInput.value;
 }
 
 let searchForm = document.querySelector("#search-form");
 
 searchForm.addEventListener("submit", search);
 
-
-// city search //
-
-
-
-let searchCity = document.querySelector("#city-input");
